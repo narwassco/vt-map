1224a52930c4b7cd9a7ea49d0a879a7f788d5021
Jin IGARASHI <wushilan0623@gmail.com>
2020-07-18 11:44:27 +0900

    shifted mbtiles2pbf module to watergis/mbtiles2pbf package

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const postgis2mbtiles = require('postgis2mbtiles');
-const Mbtiles2pbf = require('mbtiles2pbf');
+const {Mbtiles2Pbf} = require('@watergis/mbtiles2pbf');
 const {postgis2geojson} = require('@watergis/postgis2geojson');
 const config = require('./config');
 const configSearch = require('./config-search');
@@ -10,24 +10,18 @@ const VtMapCreator = () =>{
     createGeoJSONSearch();
 
     const pg2mbtiles = new postgis2mbtiles(config);
-    pg2mbtiles.run().then(res=>{
-        console.log(res);
-        //create mapbox vectortile
-        const mbtiles2pbf = new Mbtiles2pbf(config.mbtiles, config.ghpages.tiles);
-        mbtiles2pbf.run();
-
-    }).catch(err=>{
-        console.log(err);
-    }).finally(()=>{
+    const mbtiles2pbf = new Mbtiles2Pbf(config.mbtiles, config.ghpages.tiles, ".mvt");
+    pg2mbtiles.run()
+    .then(res=>{return mbtiles2pbf.run()})
+    .catch(err=>{console.log(err);}).finally(()=>{
         console.timeEnd('vt-map');
     })
 };
 
 const createGeoJSONSearch = async() =>{
     const pg2json = new postgis2geojson(configSearch);
-    await pg2json.run().then(res=>{
-        res.forEach(f=>{console.log(f)});
-    }).catch(err=>{console.log(err)});
+    const files = await pg2json.run();
+    files.forEach(f=>{console.log(f)});
 }
 
 module.exports = VtMapCreator();
